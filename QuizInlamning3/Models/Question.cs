3566f47a431143732ac9fcf6fc62cef81ad0726a
Daniel Ränk <59202381+danirank@users.noreply.github.com>
2025-10-11 09:05:52 +0200

    Add project files.

---
+++ QuizInlamning3/Models/Question.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizInlamning3.Models
+{
+    public class Question
+    {
+        public string QuestionText { get; set; }
+
+        public string[] Answers { get; set; }
+
+        public int CorrectAnswerIndex {  get; set; }
+
+        public Question(string questionText, string[]answers, int correctAnsIndex) 
+        {
+            QuestionText = questionText;
+            Answers = answers;
+            CorrectAnswerIndex = correctAnsIndex;
+
+        }
+
+    }
+}
